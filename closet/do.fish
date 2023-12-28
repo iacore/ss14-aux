19f22edb74a224ab7e6012b01a29060feff5fb18
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-12-28 13:08:16 +0000

    update script

--- closet/do.fish
+++ closet/do.fish
@@ -1,15 +1,18 @@
 #!/usr/bin/fish
 #
-set root "../space-station-14/Resources/Prototypes/Entities/Structures/Storage/Closets/"
+set root ~/"computing/game/space-station-14/Resources/Prototypes/Entities/Structures/Storage/Closets/"
 
 set files $root/**.yml
 
 true > staging
 for f in $files
-	yq 'map((.parent+" -> "+.id))' < $f >> staging
+	echo processing $f
+	# there was once a garbage software called PyYAML and it cannot ignore tags
+	yq < $f | ./extract >> staging
+	#./extract < $f >> staging
 end
 
 echo "digraph {" > out.dot
-jq -sr 'reduce .[] as $x ([]; . + $x)|.[]' < staging >> out.dot
+cat staging >> out.dot
 echo "}" >> out.dot
 
